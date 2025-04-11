3a8437e33348a1ed9e02dcd10ee6e236060fdad0
Glitch <garyneedham1@gmail.com>
2025-04-11 18:03:26 -0500

    Add GlobalSuppressions.cs for code analysis suppressions

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+[assembly: SuppressMessage("Method Declaration", "Harmony003:Harmony non-ref patch parameters modified", Justification = "<Pending>", Scope = "member", Target = "~M:DiscordHook.Patches.SteamManagerPatches.OnJoinLobby(Steamworks.Data.Lobby)")]
+[assembly: SuppressMessage("Method Declaration", "Harmony003:Harmony non-ref patch parameters modified", Justification = "<Pending>", Scope = "member", Target = "~M:DiscordHook.Patches.SteamManagerPatches.OnLobbyJoined(Steamworks.Data.Lobby,Steamworks.Friend)")]
+[assembly: SuppressMessage("Method Declaration", "Harmony003:Harmony non-ref patch parameters modified", Justification = "<Pending>", Scope = "member", Target = "~M:DiscordHook.Patches.SteamManagerPatches.OnLobbyLeft(Steamworks.Data.Lobby,Steamworks.Friend)")]
