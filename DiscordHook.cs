53e150784f88868cb048f63b2dc8bc8a0c18c4a1
Glitch <garyneedham1@gmail.com>
2025-04-11 18:03:14 -0500

    Refactor DiscordHook class to streamline patching process and improve code organization

--- DiscordHook.cs
+++ DiscordHook.cs
@@ -1,26 +1,21 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
-using UnityEngine;
+using DiscordHook.Utils;
 using HarmonyLib;
 using System;
-using System.Linq;
-using Steamworks;
+using UnityEngine;
 
 namespace DiscordHook;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 internal sealed class DiscordHook : BaseUnityPlugin
 {
     internal static DiscordHook Instance { get; private set; } = null!;
     internal new static ManualLogSource Logger => Instance._logger;
     private ManualLogSource _logger => base.Logger;
-    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
-
+    internal Harmony? Harmony { get; set; }
     internal static DiscordHookConfig? BoundConfig { get; private set; }
-
-    private static Type[] CombinedPatchTypes => 
-    LobbyPatchTypesHolder.LobbyPatches.Concat(PlayerPatchTypesHolder.PlayerPatches).ToArray();
-
     private void Awake()
     {
         if (Instance == null) Instance = this;
@@ -34,41 +29,41 @@ internal sealed class DiscordHook : BaseUnityPlugin
 
         this.gameObject.transform.parent = null;
         this.gameObject.hideFlags = HideFlags.HideAndDontSave;
-
-
-        foreach (var patchType in CombinedPatchTypes)
-        {
-            try
-            {
-                harmony.PatchAll(patchType);
-            }
-            catch (Exception)
-            {
-                Logger.LogError($"Failed to patch {patchType.Name}: {Environment.NewLine}{Environment.StackTrace}");
-                return;
-            }
-
-        }
-
+        Patch();
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
 
-        PostDiscordMessage(message: $"{SteamClient.Name} has started playing!", embed: new DiscordEmbed { Title = "Plugin Loaded", Description = $"{SteamClient.Name} has started playing!" });
     }
 
-    internal void PostDiscordMessage(string message, DiscordEmbed embed)
+    internal void Patch()
     {
-        var username = BoundConfig?.WebhookUsername.Value!;
-        var avatarUrl = BoundConfig?.WebhookAvatarUrl.Value!;
-        var bannerUrl = BoundConfig?.WebhookBannerUrl.Value!;
-        if (BoundConfig?.SendAsEmbed.Value == true && embed != null)
+        if (Harmony == null)
         {
-            StartCoroutine(new DiscordWebhook().SendWebhook(username, avatarUrl, bannerUrl, embed: embed));
+            Harmony val = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            Harmony val2 = val;
+            Harmony = val;
         }
-        else
+        Harmony.PatchAll();
+    }
+    internal void Unpatch()
+    {
+        Harmony? harmony = Harmony;
+        if (harmony != null)
         {
-            StartCoroutine(new DiscordWebhook().SendWebhook(username, avatarUrl, bannerUrl, embed: null, message: message));
+            harmony.UnpatchSelf();
         }
     }
+    private void Update()
+    {
+
+    }
+    private void OnDestroy()
+    {
+        Unpatch();
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_NAME} v{MyPluginInfo.PLUGIN_VERSION} has been unloaded!");
+    }
+
+    
 }
+
 
 
